3d4c5476a126d93193b5363a3a095ac381c6fcfe
Cihan Mert PEKER <c.mertpeker@hotmail.com>
2023-09-24 00:21:54 +0300

    Add project files.

---
+++ CihanPortfolio/Entities/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CihanPortfolio.Entities
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int Rate { get; set; }
+    }
+
+}
---
+++ CihanPortfolio/Extensions/ListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CihanPortfolio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CihanPortfolio.Extensions
+{
+    public static class ListExtensions
+    {
+        
+        public static void AddDuplicate<T>(this List<T> list, T item) where T : class
+        {
+            list.Add(item);
+            list.Add(item);
+        }
+    }
+}
